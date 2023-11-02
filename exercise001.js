748902e0fd5afc88d6e58a653716ec848cd31d09
trBizhan <145677592+trBizhan@users.noreply.github.com>
2023-11-02 08:33:14 +0000

    Fix: exercise001.js with arrow function and additional note for future ref

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,36 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve("done"), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject("error"), 2000);
+  }
 });
 
-// Your solution(s) to exercise001 here!
+promise.then(
+  (value) => console.log(`Yay! Promise resolved with response: ${value}`),
+  (reason) => console.log(`Boo. Promise rejected with response: ${reason}`)
+);
+
+/* ==============
+// first iteration with some description:
+// value and reason below can be any name
+// handleResolve and handleReject need to be part of promise then
+function handleResolve(value) {
+  console.log(`Yay! Promise resolved with response: ${value}`);
+}
+
+function handleReject(reason) {
+  console.error(`Boo. Promise rejected with response: ${reason}`);
+}
+
+// handleResolve and handleReject need to be part of promise then
+// you can name them as you wish, they just have to relate to the
+// function names above
+promise.then(handleResolve, handleReject);
+
+*=================*/
