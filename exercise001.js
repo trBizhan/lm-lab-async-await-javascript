9f33946d936174aa4d2a58a212ba2c4244daa6b4
trBizhan <145677592+trBizhan@users.noreply.github.com>
2023-11-02 09:45:31 +0000

    Fix: exercise001, async/await and promise with try/catch

--- exercise001.js
+++ exercise001.js
@@ -1,3 +1,4 @@
+/*   original promise
 const promise = new Promise((resolve, reject) => {
   // Generate random number between 0 and 9
   const randomInt = Math.floor(Math.random() * 10);
@@ -10,28 +11,38 @@ const promise = new Promise((resolve, reject) => {
     setTimeout(() => reject("error"), 2000);
   }
 });
+********************************/
 
+// use async / await
+
+const getPromiseResponse = async () => {
+  try {
+    await new Promise((resolve, reject) => {
+      // Generate random number between 0 and 9
+      const randomInt = Math.floor(Math.random() * 10);
+
+      if (randomInt % 2 === 0) {
+        // Success
+        setTimeout(() => resolve("done"), 2000);
+      } else {
+        // Failure
+        setTimeout(() => reject("error"), 2000);
+      }
+    });
+
+    console.log(`Yay! Promise resolved with response: done`);
+  } catch (reason) {
+    console.log(`Boo. Promise rejected with response: error`);
+  }
+};
+
+getPromiseResponse();
+
+/*
 promise
   .then((value) => console.log(`Yay! Promise resolved with response: ${value}`))
   .catch((reason) =>
     console.log(`Boo. Promise rejected with response: ${reason}`)
   );
 
-/* ==============
-// first iteration with some description:
-// value and reason below can be any name
-// handleResolve and handleReject need to be part of promise then
-function handleResolve(value) {
-  console.log(`Yay! Promise resolved with response: ${value}`);
-}
-
-function handleReject(reason) {
-  console.error(`Boo. Promise rejected with response: ${reason}`);
-}
-
-// handleResolve and handleReject need to be part of promise then
-// you can name them as you wish, they just have to relate to the
-// function names above
-promise.then(handleResolve, handleReject);
-
-*=================*/
+  */
